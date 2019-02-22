2e922a3010d8f6290c915afc2132e084bdd5a221
S534687 <chaturk31@gmail.com>
2019-02-22 16:46:42 -0600

    commit

--- test.js
+++ test.js
@@ -16,6 +16,11 @@ QUnit.test( "TestArea", function( assert )
             var res = area(4,3);
             assert.equal( res, "6", "Area of Rhombus with these inputs will be 6" );
 });
+QUnit.test( "TestArea", function( assert ) 
+{
+            var res = area(3,6);
+            assert.equal( res, "7", "Area of Rhombus with these inputs will be 7" );
+});
 
 QUnit.test( "TestArea", function( assert ) 
 {
